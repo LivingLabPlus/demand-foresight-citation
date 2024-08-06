3b62db65e0f36f5f0a33fb487d6444e88f58dceb
LivingLabPlus <dataxlab@gmail.com>
2024-08-06 12:49:06 +0930

    add sheet link & modify options

--- index.py
+++ index.py
@@ -1,6 +1,4 @@
 import streamlit as st
-import random
-import time
 from langchain_conversational_rag import rag
 
 st.set_page_config(page_title="Demand Foresight")
@@ -9,17 +7,18 @@ st.title('Demand Foresight')
 #claude_api_key = st.sidebar.text_input('Claude API Key')
 select_model = st.sidebar.selectbox(
     label="模型", 
-    options=["gpt-4o", "claude-3-opus-20240229"], 
+    options=st.secrets['MODEL_OPTION'],
     index=0, 
     key="model_selection"
 )
 select_tag = st.sidebar.selectbox(
-    label="文件類別", 
-    options=["全", "AI", "能源", "樂齡", "智慧城市"], 
+    label="文件類別",  
+    options=st.secrets['TAG_OPTION'],
     index=0, 
     key="tag_selection"
 )
 temp = st.sidebar.slider("Temperature", min_value=0.00 , max_value=1.0, step=0.01, key="temperature")
+st.sidebar.link_button("測試回饋表單", st.secrets["SHEET_LINK"])
 
 # Initialize chat history
 if "messages" not in st.session_state:
